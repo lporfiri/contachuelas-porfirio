58841a0b46e296f403d2ab8511a9a204e5086df1
Leonardo Porfirio <elporfiri@gmail.com>
2021-06-11 15:30:52 -0300

    se cambia nombre en href

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="#">CONTACHUELAS<span className="sr-only">(current)</span></a>
+          <a className="nav-link" href=".\">CONTACHUELAS<span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="Ropa">Ropa</a>
